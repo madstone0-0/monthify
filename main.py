50d512317aa96b287890f0a309811c21ffd3bc4b
Madiba Hudson-Quansah <mhquansah@gmail.com>
2022-08-25 17:11:32 +0000

    Finalized runtime sequence

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from auth import Spotify
 
 if __name__ == "__main__":
     controller = Spotify()
+
+    # Get user saved tracks
     controller.get_saved_track_info()
-    controller.print_saved_track_info()
-    # controller.get_playlist_names_names()
+
+    # Generate names of playlists based on month and year saved tracks were added
+    controller.get_playlist_names_names()
+
+    # Create playlists based on month and year
+    controller.create_monthly_playlists()
+
+    # Retrieve playlist ids of created playlists
+    controller.get_monthly_playlist_ids()
+
+    # Add saved tracks to created playlists by month and year
+    controller.sort_tracks_by_month()
