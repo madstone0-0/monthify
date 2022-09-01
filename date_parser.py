1e73a6fe99af7f220dfeee73c9805032bd313155
Madiba Hudson-Quansah <mhquansah@gmail.com>
2022-09-01 23:21:15 +0000

    Moved chronological sorting to separate function

--- date_parser.py
+++ date_parser.py
@@ -9,3 +9,8 @@ def extract_month_and_year(date) -> (str, str):
     year = datem.year
     month = datem.strftime("%B")
     return str(month), str(year)
+
+
+def sort_chronologically(playlist_names) -> list[str]:
+    sorted_list = sorted(playlist_names, key=lambda d: (d[1], datetime.datetime.strptime(d[0], "%B")), reverse=True)
+    return sorted_list
