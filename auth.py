ee421573c9ff25f5a7bd7cb370e0bd61e629901e
Madiba Hudson-Quansah <mhquansah@gmail.com>
2022-12-20 00:23:24 +0000

    (fix): Improved auth process

--- auth.py
+++ auth.py
@@ -2,29 +2,31 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+MAX_TRIES = 5
+
 
 class Auth:
     def __init__(self, CLIENT_ID, CLIENT_SECRET):
         self.client_secret = CLIENT_SECRET
         self.client_id = CLIENT_ID
         self.redirect_uri = "https://open.spotify.com/"
-        self.scope_read = "user-library-read"
-        self.scope_read_private_playlist = "playlist-read-private"
-        self.scope_modify_private_playlist = "playlist-modify-private"
+        self.scopes = [
+            "user-library-read",
+            "playlist-read-private",
+            "playlist-modify-private",
+        ]
 
-    def spotipy_init(self, *scope):
+    def spotipy_init(self, scopes):
         return spotipy.Spotify(
+            retries=MAX_TRIES,
+            requests_timeout=10,
             auth_manager=SpotifyOAuth(
                 client_id=self.client_id,
                 client_secret=self.client_secret,
                 redirect_uri=self.redirect_uri,
-                scope=scope,
-            )
+                scope=[str(scope) for scope in scopes],
+            ),
         )
 
     def get_spotipy(self) -> spotipy.Spotify:
-        return self.spotipy_init(
-            self.scope_read,
-            self.scope_read_private_playlist,
-            self.scope_modify_private_playlist,
-        )
+        return self.spotipy_init(self.scopes)
