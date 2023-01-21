9013a0a30c86e61ffe06258d41e0b126218eaa74
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-01-21 17:38:57 +0000

    (chore): Changed track repr

--- monthify/track.py
+++ monthify/track.py
@@ -23,11 +23,10 @@ class Track:
     def __repr__(self):
         cls = self.__class__
         cls_name = cls.__name__
-        indent = " " * 4
         res = [f"{cls_name}("]
         for f in fields(cls):
             val = getattr(self, f.name)
-            res.append(f"{indent}{f.name} = {val!r}")
+            res.append(f" {f.name} = {val!r}")
 
         res.append(")")
-        return "\n".join(res)
+        return "".join(res)
